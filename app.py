9b38fef386e400d279943e6567acca311546b539
Mouni0816 <74076319+Mouni0816@users.noreply.github.com>
2021-09-13 16:59:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
 import streamlit as st
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from transformers import pipeline, AutoModelForTokenClassification, AutoTokenizer
 
+# Sentiment analysis pipeline
+
+
+# Question answering pipeline, specifying the checkpoint identifier
+model = pipeline('sentiment-analysis', model='distilbert-base-uncased-finetuned-sst-2-english', tokenizer='distilbert-base-uncased-finetuned-sst-2-english')
+
+# Named entity recognition pipeline, passing in a specific model and tokenizer
 st.title('Hello World!')
 
-tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
 
-model = AutoModelForSequenceClassification.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
 form = st.form(key='my_form')
 text = form.text_input(label='Enter some text')
 submit_button = form.form_submit_button(label='Submit')
 
 if submit_button:
     st.subheader('Data')
-    inputs = tokenizer(text, return_tensors="pt")
-    outputs = model(**inputs)
+    outputs = model(text)
     st.write({"emotion": outputs})
