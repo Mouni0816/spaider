b3c9104b9dfb193aaa697b9ee5af395b73fc9a9d
Mouni0816 <74076319+Mouni0816@users.noreply.github.com>
2021-09-13 15:27:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 import streamlit as st
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
 st.title('Hello World!')
 
+tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
+
+model = AutoModelForSequenceClassification.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
 form = st.form(key='my_form')
 text = form.text_input(label='Enter some text')
 submit_button = form.form_submit_button(label='Submit')
 
 if submit_button:
     st.subheader('Data')
-    st.write({"text": text})
+    inputs = tokenizer(text, return_tensors="pt")
+    outputs = model(**inputs)
+    st.write({"emotion": outputs})
